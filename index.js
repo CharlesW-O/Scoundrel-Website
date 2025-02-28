98ab0ad69c68e1db4649ef608c01e65cba77d318
Charles Wehner-Ortega <cwwo@wehnerortega.net>
2025-02-28 16:05:36 -0700

    Temp layout + deck randomization

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // alert("working");
+
+// Sets up initial dungeon/deck array (not randomized).
+
+const dungeonDeck = [];
+
+function reloadDeck() {
+    for (i = 1; i <= 44; i++) {
+        dungeonDeck.push(i);
+    }
+}
+
+reloadDeck();
+
+// Randomizes the dungeon/deck
+function shuffleDeck() {
+    for (let i = 0; i < dungeonDeck.length; i++) {
+        let shuffle = Math.floor(Math.random() * (dungeonDeck.length));
+        [dungeonDeck[i], dungeonDeck[shuffle]] = [dungeonDeck[shuffle], dungeonDeck[i]];
+    }
+}
