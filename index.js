347f48814aa38a99d1dcfd864f1e831515722fdc
Charles Wehner-Ortega <cwwo@wehnerortega.net>
2025-03-04 10:34:18 -0700

    Track room card value in img class + phase 1 of dragging

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let fledTwice = false;
+
 //Resets game state - need to add to as keep coding (for health etc).//
 window.onload = function() {
     resetGame();
@@ -19,6 +21,7 @@ function resetGame () {
 
     //Reset fled tracker
     fledTwice = false; 
+    $("#flee-btn").attr("src", "./images/FleecoinOntest.png");
 
     //Reset health to full
     $("#health-img").attr("src","./images/Health/20health.png");
@@ -66,9 +69,11 @@ function checkRoomOpenings () {
 }
 
 function refillRoom () { //need to figure out dragging cards triggering this before can fully finish the partial room reloads.
+
     switch (checkRoomOpenings()) {
         case 1:
             // partiallyPopulateRoom(2, 3, 4);
+            removeClassValues();
             room2 = dungeonDeck.shift();
             room3 = dungeonDeck.shift();
             room4 = dungeonDeck.shift();
@@ -76,6 +81,7 @@ function refillRoom () { //need to figure out dragging cards triggering this bef
             
         case 2:
             // partiallyPopulateRoom(1, 3, 4);
+            removeClassValues();
             room1 = dungeonDeck.shift();
             room3 = dungeonDeck.shift();
             room4 = dungeonDeck.shift();
@@ -83,6 +89,7 @@ function refillRoom () { //need to figure out dragging cards triggering this bef
     
         case 3:
             // partiallyPopulateRoom(1, 2, 4);
+            removeClassValues();
             room1 = dungeonDeck.shift();
             room2 = dungeonDeck.shift();
             room4 = dungeonDeck.shift();
@@ -90,6 +97,7 @@ function refillRoom () { //need to figure out dragging cards triggering this bef
     
         case 4:
             // partiallyPopulateRoom(1, 2, 3);
+            removeClassValues();
             room1 = dungeonDeck.shift();
             room2 = dungeonDeck.shift();
             room3 = dungeonDeck.shift();
@@ -117,6 +125,8 @@ function refillRoom () { //need to figure out dragging cards triggering this bef
 // }
 
 function fullyPopulateRoom() {
+    removeClassValues();
+
     room1 = dungeonDeck.shift();
     room2 = dungeonDeck.shift();
     room3 = dungeonDeck.shift();
@@ -134,18 +144,92 @@ function fullyPopulateRoom() {
 }
 
 function changeRoomImgs () {
+
     $("#room-1-img").attr("src","./images/Temp Cards/" + room1 + ".png" );
     $("#room-2-img").attr("src","./images/Temp Cards/" + room2 + ".png" );
     $("#room-3-img").attr("src","./images/Temp Cards/" + room3 + ".png" );
     $("#room-4-img").attr("src","./images/Temp Cards/" + room4 + ".png" );
+
+    document.querySelector("#room-1-img").classList.add(room1);
+    document.querySelector("#room-2-img").classList.add(room2);
+    document.querySelector("#room-3-img").classList.add(room3);
+    document.querySelector("#room-4-img").classList.add(room4);
+
+    console.log(document.querySelector("#room-1-img"));
+    console.log(document.querySelector("#room-2-img"));
+    console.log(document.querySelector("#room-3-img"));
+    console.log(document.querySelector("#room-4-img"));
+}
+
+function removeClassValues () {
+    document.querySelector("#room-1-img").classList.remove(room1);
+    document.querySelector("#room-2-img").classList.remove(room2);
+    document.querySelector("#room-3-img").classList.remove(room3);
+    document.querySelector("#room-4-img").classList.remove(room4);
 }
 
 //Weapon Array
 
 //Discard Array
 
+// Draggable Cards
+let dragged;
+const dragCard = $(".draggable");
+const dropTarget = $(".drop-target")
+
+    //Activates when dragging
+dragCard.on("drag", (event) => {
+    console.log("dragging");
+} );
+
+dragCard.on("dragstart", (event) => {
+    /* store a ref. on the dragged elem */
+    dragged = event.target;
+    event.target.classList.add("dragging");
+  });
+
+dragCard.on("dragend", (event) => {
+    event.target.classList.remove("dragging");
+});
+
+    // Events fired on the drop targets
+
+dropTarget.on("dragover", (event) => {
+    /* prevent default to allow drop */
+    event.preventDefault();
+  },
+  false,
+);
+
+dropTarget.on("dragenter", (event) => {
+    if (event.target.classList.contains("drop-target")) {
+      event.target.classList.add("drag-over");
+    }
+  });
+
+dropTarget.on("dragleave", (event) => {
+  if (event.target.classList.contains("drop-target")) {
+    event.target.classList.remove("drag-over");
+  }
+});
+
+dropTarget.on("drop", (event) => {
+    /* prevent default action (open as link for some elements) */
+    event.preventDefault();
+    /* move dragged element to the selected drop target */
+    if (event.target.classList.contains("drop-target")) {
+      event.target.classList.remove("drag-over");
+    
+    /* replace target img with new img*/
+      event.target.src = dragged.src;
+      dragged.src = "./images/Temp Cards/0.png";
+    }
+  });
+
+
+
 //Flee Button
-let fledTwice = false;
+
 
 $("#flee").on("click", function flee () {
     if (fledTwice == false && room1 > 0 && room2 > 0 && room3 > 0 && room4 > 0) {
